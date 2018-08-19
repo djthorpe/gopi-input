9e910ac5afa5774e7226f7577667624a76aaa51c
David Thorpe <djt@mutablelogic.com>
2018-08-19 11:15:39 +0100

    Updated v1

--- cmd/input-service.go
+++ cmd/input-service.go
@@ -9,10 +9,23 @@ import (
 
 	// Modules
 	_ "github.com/djthorpe/gopi-input/sys/input"
+	_ "github.com/djthorpe/gopi/sys/logger"
 )
 
 func Main(app *gopi.AppInstance, done chan<- struct{}) error {
 	fmt.Println(app.Input)
+
+	// All inputs
+	if devices, err := app.Input.OpenDevicesByName("", gopi.INPUT_TYPE_ANY, gopi.INPUT_BUS_ANY); err != nil {
+		return err
+	} else {
+		for i, d := range devices {
+			fmt.Println(i, d)
+		}
+	}
+
+	done <- gopi.DONE
+	return nil
 }
 
 func main() {
